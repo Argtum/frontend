a0985f6e4a5c0f4045ce87fdfad1cb91d17d4bd6
Alexey Serebryakov <Silwar40@gmail.com>
2018-04-09 00:09:09 +0300

    Добавил ещё 4 фильма для JS

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var hiddenMovieContainer = document.body.getElementsByClassName('my_movies_container_two');
+if (document.body.getElementsByClassName('all_movies_button').click)
+{
+	hiddenMovieContainer.style.display = 'block';
+}
