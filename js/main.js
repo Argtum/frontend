66aca00dc6954b355c43d2532649e0ee8ff1c6bc
Alexey Serebryakov <Silwar40@gmail.com>
2018-04-14 16:03:09 +0300

    подготовка в JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-var hiddenMovieContainer = document.body.getElementsByClassName('my_movies_container_two');
-if (document.body.getElementsByClassName('all_movies_button').click)
+window.onload = function()
 {
-	hiddenMovieContainer.style.display = 'block';
+	var movieButton = document.getElementsByClassName('all_movies_button')[0];
+	var hiddenMovieContainer = document.getElementsByClassName('my_movies_container_two')[0];
+
+	//	movieButton.onclick = function()
+	movieButton.addEventListener('click', function()
+	{
+		movieButton.style.display = 'none';
+		movieButton.style.height = '0';
+		hiddenMovieContainer.style.display = 'block';
+		hiddenMovieContainer.style.opacity = '0';
+		hiddenMovieContainer.style.opacity = '1';
+	});
 }
