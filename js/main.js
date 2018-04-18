a962cd2cfc33d41a47c525c4e5af2211f2928def
Alexey Serebryakov <Silwar40@gmail.com>
2018-04-18 22:54:21 +0300

    Скрипт для второго блока фильма

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
+function showMoreFilms(event)
+{
+    event.preventDefault();
+    var hiddenMovieContainer = document.getElementById("hidden_movie_container");
+    
+    document.getElementById("all_movies_button").style.display = "none";
+    hiddenMovieContainer.classList.add("display_movie_container");
+    setTimeout(function()
+    {
+        hiddenMovieContainer.classList.add("opacity_movie_container");
+    }, 1);
+}
+
+
+
 window.onload = function()
 {
-	var movieButton = document.getElementsByClassName('all_movies_button')[0];
-	var hiddenMovieContainer = document.getElementsByClassName('my_movies_container_two')[0];
-
-	//	movieButton.onclick = function()
-	movieButton.addEventListener('click', function()
-	{
-		movieButton.style.display = 'none';
-		movieButton.style.height = '0';
-		hiddenMovieContainer.style.display = 'block';
-		hiddenMovieContainer.style.opacity = '0';
-		hiddenMovieContainer.style.opacity = '1';
-	});
+    document.getElementById("all_movies_button").onclick = showMoreFilms;
 }
